32657ec7587d6a7c7feac9f55cf95160abf6ca57
Jim Thorne <jamesthorne@Jamess-MacBook-Air.local>
2019-12-17 12:40:18 -0700

    first commit

---
+++ number10.js
@@ -0,0 +1,42 @@
+// Addition Operator
+let addition = 100 + 60;
+console.log (addition);
+// Expected Result 160
+
+// Subtraction operator
+let subtraction = 100 - 60;
+console.log (subtraction);
+// Expected Result 40
+
+// Multiplication operator
+let multiply = 60 * 10;
+console.log (multiply);
+// Expected Result 600
+
+// Division operator
+let divide = 1000 / 50;
+console.log (divide);
+// Expected result 20
+
+//Remainder operator
+let remainder = 55 % 2;
+console.log (remainder);
+// Expected result 1 
+
+// Increment operator
+let number = 10;
+number ++; 
+// Increment = Increment +1
+console.log (number);
+// Expected result = 11; Only increases by 1 each iteration
+
+//Decrement = Current number value -1
+number --;
+console.log (number)
+// Expected result = 10; Decreases by one to previous iteration
+
+
+
+
+
+
